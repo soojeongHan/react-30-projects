7d5c97cbf0e0deef20f085a383ae3f573132b344
SoojeongHan <gkstnwjd8420@gmail.com>
2021-02-04 17:39:54 +0900

    remove Element.classList.add() & .remove() & create Class Update & update README.md

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # React 30 Projects
 
-- React 30 projects based on Javascript 30 projects([LINK]('https://github.com/soojeongHan/js-30-projects'))
-- Testing Page - Github Pages Deploy([LINK]('https://soojeonghan.github.io/react-30-projects/'))
-- React 30 projects Description([LINK]('https://www.notion.so/soojeonghan/React-30-Projects-Description-a5de103e11b748a1a7a87eedf2a5b8cd'))
-- Javascript 30 projects Lecture([LINK]('https://courses.wesbos.com/account'))
+- React 30 projects based on Javascript 30 projects(_[LINK](https://github.com/soojeongHan/js-30-projects)_)
+- Testing Page - Github Pages Deploy(_[LINK](https://soojeonghan.github.io/react-30-projects/)_)
+- React 30 projects Description (_[NOTION_LINK](https://www.notion.so/soojeonghan/React-30-Projects-Description-a5de103e11b748a1a7a87eedf2a5b8cd)_)
+- Javascript 30 projects Lecture _By wesbos_ (_[LINK](https://courses.wesbos.com/account)_)
 
 ## List
 
--- src/projects/1/Index.js
+++ src/projects/1/Index.js
@@ -4,18 +4,19 @@ import sounds from "./sounds/sounds";
 
 const Index = () => {
   const divRef = React.useRef();
-  // 컴포넌트를 로딩하면, 키보드를 바로 눌러도 효과를 낼 수 있게 focus() 처리한다.
+  // 컴포넌트를 로딩하면, 키보드를 바로 눌러도 소리를 재생할 수 있도록 focus() 처리한다.
   React.useLayoutEffect(() => {
     divRef.current.focus();
   }, []);
+  const [key, setKey] = React.useState("");
 
   // transition이 끝나면, playing 클래스를 제거한다.
   const removeTranstion = (e) => {
     if (e.propertyName !== "transform") return;
-    e.target.classList.remove("playing");
+    setKey("");
   };
 
-  function onKeyDown(e) {
+  const onKeyDown = (e) => {
     // drum 클래스의 keys 클래스의 key 값들을 배열로 만들어, e.keyCode값과 dataset의 값이 일치하는 값을 찾아낸다.
     const key = Array.from(divRef.current.firstChild.childNodes).find((v) => {
       return v.dataset.key === String(e.keyCode);
@@ -32,8 +33,12 @@ const Index = () => {
     if (!audio) return;
     // audio를 재생하고, playing 클래스를 추가하여, transition 효과를 낸다.
     audio.play();
-    key.classList.add("playing");
-  }
+    setKey(key.dataset.key);
+  };
+  // data_key와 key가 같으면 playing class를 추가한다.
+  const isKey = (data_key) => {
+    return key === data_key ? "playing" : "";
+  };
 
   return (
     <div
@@ -43,39 +48,39 @@ const Index = () => {
       ref={divRef}
     >
       <div className="keys" onTransitionEnd={removeTranstion}>
-        <div data-key="65" className="key">
+        <div data-key="65" className={`key ${isKey("65")}`}>
           <kbd>A</kbd>
           <span className="sound">clap</span>
         </div>
-        <div data-key="83" className="key">
+        <div data-key="83" className={`key ${isKey("83")}`}>
           <kbd>S</kbd>
           <span className="sound">hihat</span>
         </div>
-        <div data-key="68" className="key">
+        <div data-key="68" className={`key ${isKey("68")}`}>
           <kbd>D</kbd>
           <span className="sound">kick</span>
         </div>
-        <div data-key="70" className="key">
+        <div data-key="70" className={`key ${isKey("70")}`}>
           <kbd>F</kbd>
           <span className="sound">openhat</span>
         </div>
-        <div data-key="71" className="key">
+        <div data-key="71" className={`key ${isKey("71")}`}>
           <kbd>G</kbd>
           <span className="sound">boom</span>
         </div>
-        <div data-key="72" className="key">
+        <div data-key="72" className={`key ${isKey("72")}`}>
           <kbd>H</kbd>
           <span className="sound">ride</span>
         </div>
-        <div data-key="74" className="key">
+        <div data-key="74" className={`key ${isKey("74")}`}>
           <kbd>J</kbd>
           <span className="sound">snare</span>
         </div>
-        <div data-key="75" className="key">
+        <div data-key="75" className={`key ${isKey("75")}`}>
           <kbd>K</kbd>
           <span className="sound">tom</span>
         </div>
-        <div data-key="76" className="key">
+        <div data-key="76" className={`key ${isKey("76")}`}>
           <kbd>L</kbd>
           <span className="sound">tink</span>
         </div>
