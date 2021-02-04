d5ea38090c12159ae1423568ec15dfb3456b42f8
SoojeongHan <gkstnwjd8420@gmail.com>
2021-02-04 16:27:15 +0900

    remove querySelector Function & update README.md

--- src/projects/1/Index.js
+++ src/projects/1/Index.js
@@ -4,26 +4,33 @@ import sounds from "./sounds/sounds";
 
 const Index = () => {
   const divRef = React.useRef();
+  // 컴포넌트를 로딩하면, 키보드를 바로 눌러도 효과를 낼 수 있게 focus() 처리한다.
   React.useLayoutEffect(() => {
     divRef.current.focus();
-    const keys = Array.from(document.querySelectorAll(".keys > .key"));
-    keys.forEach((key) =>
-      key.addEventListener("transitionend", removeTranstion)
-    );
   }, []);
+
+  // transition이 끝나면, playing 클래스를 제거한다.
   const removeTranstion = (e) => {
     if (e.propertyName !== "transform") return;
     e.target.classList.remove("playing");
   };
+
   function onKeyDown(e) {
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    // drum 클래스의 keys 클래스의 key 값들을 배열로 만들어, e.keyCode값과 dataset의 값이 일치하는 값을 찾아낸다.
+    const key = Array.from(divRef.current.firstChild.childNodes).find((v) => {
+      return v.dataset.key === String(e.keyCode);
+    });
+    // key 값이 없으면 return;
     if (!key) return;
+    // span tag의 InnerHTML의 값으로 sounds 배열 중 일치하는 값을 찾는다.
     const audio = new Audio(
       sounds.find((v) => {
-        return String(v).includes(key.querySelector(".sound").innerHTML);
+        return String(v).includes(key.lastChild.innerHTML);
       })
     );
+    // audio 값이 없으면 return;
     if (!audio) return;
+    // audio를 재생하고, playing 클래스를 추가하여, transition 효과를 낸다.
     audio.play();
     key.classList.add("playing");
   }
@@ -35,7 +42,7 @@ const Index = () => {
       tabIndex="0"
       ref={divRef}
     >
-      <div className="keys">
+      <div className="keys" onTransitionEnd={removeTranstion}>
         <div data-key="65" className="key">
           <kbd>A</kbd>
           <span className="sound">clap</span>
